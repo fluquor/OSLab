fb318d1234619481571487e908f73a8466f5336c
asahi <hty980204@gmail.com>
2019-05-31 14:49:27 +0800

    complete fp-growh; need debuged

--- fp-growth/FPTree.go
+++ fp-growth/FPTree.go
@@ -37,40 +37,40 @@ func (t FPTree) Items() map[ItemType][]*FPNode {
 	return result
 }
 func (t *FPTree) Nodes(item ItemType) []*FPNode {
-	if route, ok := t.routes[item]; !ok {
+	if _, ok := t.routes[item]; !ok {
 		return []*FPNode{}
-	} else {
-		result := make([]*FPNode, 0)
-		node := route[0]
-		result = append(result, node)
-		for node != nil {
-			node = node.Neighber
-		}
-		return result
 	}
+	route := t.routes[item]
+	result := make([]*FPNode, 0)
+	node := route[0]
+	result = append(result, node)
+	for node != nil {
+		node = node.Neighber
+	}
+	return result
 }
 
 func (t *FPTree) PrefixPaths(item ItemType) [][]*FPNode {
-	collectPath := func(t *FPNode) {
-		path := make([]*FPNode)
+	collectPath := func(t *FPNode) []*FPNode {
+		path := make([]*FPNode, 0)
 		for t != nil && !t.IsRoot() {
 			path = append(path, t)
 			t = t.Parent
 		}
 		for i := len(path)/2 - 1; i >= 0; i-- {
 			opp := len(path) - 1 - i
-			a[i], a[opp] = a[opp], a[i]
+			path[i], path[opp] = path[opp], path[i]
 		}
 		return path
 	}
-	result := make([][]*FPNode)
+	result := make([][]*FPNode, 0)
 	for _, node := range t.Nodes(item) {
-		append(result, collectPath(node))
+		result = append(result, collectPath(node))
 	}
 	return result
 }
-func FindFrequentItemsets(transactions []Transaction, min_supp float64, itemSetChan chan<- []ItemType) {
-	min_count := int(float64(len(transactions)) * min_supp)
+func FindFrequentItemsets(transactions []Transaction, minSuppRatio float64, itemSetChan chan<- []ItemType) {
+	minSuppCount := int(float64(len(transactions)) * minSuppRatio)
 	items := make(map[ItemType]int)
 	for _, trans := range transactions {
 		for _, item := range trans {
@@ -78,7 +78,7 @@ func FindFrequentItemsets(transactions []Transaction, min_supp float64, itemSetC
 		}
 	}
 	for item, count := range items {
-		if count < min_count {
+		if count < minSuppCount {
 			delete(items, item)
 		}
 	}
@@ -93,7 +93,7 @@ func FindFrequentItemsets(transactions []Transaction, min_supp float64, itemSetC
 		return newTrans
 	}
 
-	master := FPTree{}
+	master := &FPTree{}
 	for _, trans := range transactions {
 		master.Add(cleanTrans(trans))
 	}
@@ -106,7 +106,7 @@ func FindFrequentItemsets(transactions []Transaction, min_supp float64, itemSetC
 			for _, node := range nodes {
 				support += node.Count
 			}
-			if support > min_count {
+			if support > minSuppCount {
 				for _, v := range suffix {
 					if item == v {
 						continue
@@ -121,10 +121,12 @@ func FindFrequentItemsets(transactions []Transaction, min_supp float64, itemSetC
 
 		}
 	}
+
+	findWithSuffix(master, []ItemType{}, itemSetChan)
 }
 
-func ConditionalTreeFromPaths(paths [][]*FPNode) {
-	tree := FPTree{}
+func ConditionalTreeFromPaths(paths [][]*FPNode) *FPTree {
+	tree := &FPTree{}
 	var condItem = NilItem
 	items := make(map[ItemType]bool)
 	for _, path := range paths {
@@ -140,10 +142,19 @@ func ConditionalTreeFromPaths(paths [][]*FPNode) {
 				if node.Item == condItem {
 					count = node.Count
 				}
-				nextPoint = &FPNode{Tree: &tree, Item: node.Item, Count: 1}
+				nextPoint = &FPNode{Tree: tree, Item: node.Item, Count: count}
 				point.Add(nextPoint)
-				tree.updateRoute()
+				tree.updateRoute(nextPoint)
 			}
+			point = nextPoint
 		}
 	}
+
+	for _, path := range tree.PrefixPaths(condItem) {
+		count := path[len(path)-1].Count
+		for i := 0; i < len(path)-1; i++ {
+			path[i].Count += count
+		}
+	}
+	return tree
 }
